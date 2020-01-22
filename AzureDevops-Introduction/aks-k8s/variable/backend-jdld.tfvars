45abc747b87cdd0aa6ed0bf537bbd994fb652ca1
Jason Cavaliere <jason@quarterspot.com>
2020-01-22 12:24:21 -0600

    initial commit

---
+++ AzureDevops-Introduction/aks-k8s/variable/backend-jdld.tfvars
@@ -0,0 +1,8 @@
+#Variables initialization
+resource_group_name = "rg-dev-noprd-tftest"
+
+storage_account_name = "storagedevnoprdtftest" #Name must be unique
+
+container_name = "tfstate"
+
+key = "aks-k8s.tfstate"
---
+++ Best-Practice/BestPractice-4/main.tf
@@ -0,0 +1,36 @@
+#Set the terraform backend
+terraform {
+  required_version = ">= 0.12.6"
+
+  backend "azurerm" {
+    storage_account_name = "infrasdbx1vpcjdld1"
+    container_name       = "tfstate"
+    key                  = "BestPractice-4.tfstate"
+    resource_group_name  = "infr-jdld-noprd-rg1"
+  }
+}
+
+#Set the Provider
+provider "azurerm" {
+  version         = ">= 1.31.0" #Use the last version tested through an Azure DevOps pipeline here : https://dev.azure.com/jamesdld23/vpc_lab/_build
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+#Call module/resource
+data "azurerm_resource_group" "infr" {
+  name = "infr-jdld-noprd-rg1"
+}
+
+#Action
+module "Add-AzureRmLoadBalancerOutboundRules-Apps" {
+  source                     = "git::https://github.com/JamesDLD/terraform.git//module/Add-AzureRmLoadBalancerOutboundRules?ref=master"
+  lbs_out                    = var.lbs_public
+  lb_out_prefix              = "bp4-"
+  lb_out_suffix              = "-publiclb1"
+  lb_out_resource_group_name = data.azurerm_resource_group.infr.name
+  lbs_tags                   = data.azurerm_resource_group.infr.tags
+}
+
